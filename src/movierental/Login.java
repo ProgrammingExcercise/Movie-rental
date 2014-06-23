b28b205c20725969fe0e00e3eace15ee521fe4b4
Acer <Acer@Acer-PC>
2014-06-23 13:41:16 +0200

    login

--- src/movierental/Login.java
+++ src/movierental/Login.java
@@ -731,7 +731,7 @@ public class Login extends javax.swing.JFrame {
                         
                         if(!(rate.equals("%"))){
                         stmt4 = conn.createStatement();
-                        rs3 = stmt4.executeQuery("SELECT *,avg(rating) as average FROM movierental.movie natural left join rates natural join haslang WHERE title LIKE '%"+ suchetext +"%' and genre LIKE '%" + gen + "%' and price >= '"+ pri + "' and ageRating <= '"+ age +"' and Language LIKE '%"+ lang +"%' group by mid having average >= "+rate+"");
+                        rs3 = stmt4.executeQuery("SELECT *,avg(rating) as average FROM movie natural left join rates natural join haslang WHERE title LIKE '%"+ suchetext +"%' and genre LIKE '%" + gen + "%' and price >= '"+ pri + "' and ageRating <= '"+ age +"' and Language LIKE '%"+ lang +"%' group by mid having average >= "+rate+"");
                         stmtSearch = conn.createStatement();
                         
                         while(rs3.next()){
