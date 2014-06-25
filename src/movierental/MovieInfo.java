05bbfc504b24b52f11dfc4cf56717ef9289244a0
Acer <Acer@Acer-PC>
2014-06-25 13:40:32 +0200

    Revert "Bux fixes"
    This reverts commit d0433bd4de0899390d9010f552834a7b870aabc4.

--- src/movierental/MovieInfo.java
+++ src/movierental/MovieInfo.java
@@ -322,7 +322,9 @@ public class MovieInfo extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jButtonRentMovieActionPerformed
 
-
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
